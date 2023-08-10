2a8b31232e9d5af444ec0eba6e99c15d74f541af
Jarvis <Jarvis@gmail.com>
2023-08-11 00:25:43 +0530

    11th August

--- Data Structure and Algorithms/dummy.cpp
+++ Data Structure and Algorithms/dummy.cpp
@@ -1,42 +1,76 @@
-#include <bits/stdc++.h>
-#include <iostream>
+#include <bits/stdc++.h>  #include<iostream>
+
+#include <algorithm>
+
 using namespace std;
 
-class Car
+class Fraction
 {
-    // Write your constructor and printCarInfo method here.
-public:
-    int noOfGear;
-    string color;
-};
+    int nm;
+    int dm;
 
-class RaceCar : public Car
-{
-    // Write your constructor and printRaceCarInfo method here.
 public:
-    int maxSpeed;
-
-    RaceCar(int gear, string color, int speed)
+    Fraction(int nm, int dm)
     {
-        this->noOfGear = gear;
-        this->color = color;
-        this->maxSpeed = speed;
+        this->nm = nm; // constructor
+        this->dm = dm;
     }
 
-    void printInfo()
+    void simplify()
     {
-        cout << "noOfGear: " << this->noOfGear << endl;
-        cout << "color: " << this->color << endl;
-        cout << "maxSpeed: " << this->maxSpeed << endl;
+        int gcd = __gcd(nm, dm);
+        this->nm = this->nm / gcd;
+        this->dm = this->dm / gcd;
     }
+
+    void Add(Fraction f)
+    {
+        int d = this->dm * f.dm;
+        int n = (d / this->dm) * this->nm + (d / f.dm) * f.nm;
+        this->nm = n;
+        this->dm = d;
+        simplify();
+    }
+
+    void Multiply(Fraction f)
+    {
+        this->nm = this->nm * f.nm;
+        this->dm = this->dm * f.dm;
+
+        simplify();
+    }
+
+    void print() { cout << this->nm << "/" << this->dm << endl; }
 };
 
 int main()
 {
-    int noOfGear, maxSpeed;
-    string color;
-    cin >> noOfGear >> color >> maxSpeed;
-    RaceCar raceCar(noOfGear, color, maxSpeed);
-    raceCar.printInfo();
+
+    int n1, d1;
+    cin >> n1 >> d1;
+    Fraction f1(n1, d1);
+    int n;
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        int choice, n2, d2;
+        cin >> choice >> n2 >> d2;
+        Fraction f2(n2, d2);
+
+        if (choice == 1)
+        {
+            f1.Add(f2);
+            f1.print();
+        }
+        else
+        {
+            f1.Multiply(f2);
+            f1.print();
+        }
+    }
+
     return 0;
 }
