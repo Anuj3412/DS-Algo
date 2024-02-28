80537d66c25053dc4aa4b64436a2dd3966ebff79
Void <anujbajaj0401@gmail.com>
2024-02-28 12:39:43 +0530

    28-02-2024

--- Data Structure and Algorithms/10 OOPs/14_Inheritance.cpp
+++ Data Structure and Algorithms/10 OOPs/14_Inheritance.cpp
@@ -3,10 +3,21 @@ using namespace std;
 
 class Human {
 
+public: 
+int age;
+int height;
+int weight;
+
 };
 
+class man : public Human{
+
+    string name;
+
+};
 int main(int argc, char const *argv[])
 {
-    
+    man A;
+    cout<<A.age; // age is defined in parent class (Human)--> inherited to child class (Man)
     return 0;
 }
--- Data Structure and Algorithms/dummy.cpp
+++ Data Structure and Algorithms/dummy.cpp
@@ -1,51 +1,16 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
+
 class hero{
-public:
-string name;
-int age;
-
-hero(){
-    cout<<"default constructor is dead "<<endl;
-}
-
-hero(string name , int age){
-    this->name = name;
-    this->age = age;
-}
-
-hero(hero & temp){
-    this->name = temp.name;
-    this->age = temp.age;
-}
-
-void setName(string name){
-    this->name = name;
-}
-
-void setAge(int age){
-    this->age = age;
-}
-
 
 
 
 };
 
+int main(){
 
-
-int main()
-{   
-
-    hero A("Anuj" , 22);
-    cout<<"A- "<<A.name<<" "<<A.age<<endl;
-    hero B(A);
-    cout<<"B- "<<B.name<<" "<<B.age<<endl;
-    A.setAge(23);
-    A.setName("Anuj Bajaj");
-    cout<<"A- "<<A.name<<" "<<A.age<<endl;
-    cout<<"B- "<<B.name<<" "<<B.age<<endl;
-
+    
+    cout<<"Size: "<<sizeof(hero);
     return 0;
 }
